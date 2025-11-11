f84ce849671d5026594723805914b370a86b2cdf
Gloomy-poplar <egor061297@gmail.com>
2025-11-11 16:43:26 +0300

    1

--- services/embeddings_service.py
+++ services/embeddings_service.py
@@ -32,7 +32,7 @@ class EmbeddingsService:
 
     def __init__(
         self,
-        model_name: str = "intfloat/multilingual-e5-small",
+        model_name: str = "intfloat/multilingual-e5-base",
         knowledge_base_path: str = os.path.join(os.path.dirname(
             __file__), "..", "data", "knowledge_base.json"),
         cache_dir: Optional[str] = None,
